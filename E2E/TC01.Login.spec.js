82ee771b5351a54a18e51c6d666b538d28eddae6
git-prk <praneeta.katkar@gmail.com>
2024-06-19 12:23:31 +0530

    add

--- E2E/TC01.Login.spec.js
+++ E2E/TC01.Login.spec.js
@@ -1,8 +1,8 @@
 const {test,expect} = require('@playwright/test')
 
-test('Verify login functionality with valid credentails',async({page})=>{
+test('Verify login with valid credentails',async({page})=>{
   
-    let dashboardEle = await page.locator('h6[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]')
+    let dbelement = await page.locator('h6[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]')
     //Step 1 ==> Visit Url 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     //Step 2 ==> Enter Username 
@@ -12,14 +12,25 @@ test('Verify login functionality with valid credentails',async({page})=>{
     //Step 4 ==> Click on login button 
     await page.locator('button[type="submit"]').click()
     //Step 5 ==> Assertion/ Validation 
-    await expect(dashboardEle).toBeVisible()
-    await expect(dashboardEle).toHaveText('Dashboard')
+    await expect(dbelement).toBeVisible()
+    await expect(dbelement).toHaveText('Dashboard')
     await expect(page.locator('img[alt="client brand banner"]')).toBeVisible()
     await expect(page).toHaveTitle('OrangeHRM')
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
 })
 
+test.only('login with invalid credentials',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.locator('input[placeholder="Username"]').fill('adminuser')
+    await page.locator('input[placeholder="Password"]').fill('adminuser123')
+    await page.locator('button[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click()
+    //await expect(page.locator('p[data-v-87fcf455]')).toBeVisible
+    await page.locator('div[class="oxd-alert oxd-alert--error"]').toBeVisible
+    await expect(page.locator('p[data-v-87fcf455]')).toHaveText('Invalid credentials')
+    await expect (page.locator('img[src="/web/images/ohrm_branding.png?v=1711595107870"]')).toBeVisible
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await expect(page).toHaveTitle('OrangeHRM')
 
-//<input data-v-1f99f73c="" class="oxd-input oxd-input--active oxd-input--error" name="username" placeholder="Username" autofocus="">
-//TagName[attr = "Value"]
-//input[name ="username"]
+
+})
+
