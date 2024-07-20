6604583bdc3c20b7f19fa75c0f3b5123e6a170ee
git-prk <praneeta.katkar@gmail.com>
2024-07-20 22:14:05 +0530

    add

---
+++ E2E/gittest.spec.js
@@ -0,0 +1,9 @@
+
+const {test,expect} = require ('@playwright/test')
+
+test('Verify dblclick in playwright',async({page})=>{
+    await page.goto('https://demoqa.com/buttons')
+    await page.locator('#doubleClickBtn').dblclick()
+    await expect(page.locator('#doubleClickMessage')).toBeVisible()
+    await expect(page.locator('#doubleClickMessage')).toHaveText('You have done a double click')
+})
